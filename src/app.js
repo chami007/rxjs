5ea41d02df1c2173075e3dc0572b5511bf88edfa
Chamal Liyanage <cliyanage@hotmail.co.uk>
2016-10-10 20:24:57 +0100

    Added buton, input, movemove events

--- src/app.js
+++ src/app.js
@@ -1,4 +1,52 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS Boiler Running...');
+const btn = $('#btn');
+const input = $('#input');
+const inputToHtml = $('#inputToHtml');
+const output = $('#output');
+
+// button Click event 
+const btnstream$ = Rx.Observable.fromEvent(btn, 'click');
+
+btnstream$.subscribe(
+    function (e) { // success
+        console.log(e.target.innerHTML); // get button Name.
+    },
+    function (err) { // error
+
+    },
+    function () { //completed
+        console.log('Button click Completed');
+    }
+);
+
+// input event
+const inputstream$ = Rx.Observable.fromEvent(input, 'keyup');
+
+inputstream$.subscribe(
+    function (e) { // success
+        inputToHtml.html(e.currentTarget.value); // get button Name.
+    },
+    function (err) { // error
+
+    },
+    function () { //completed
+        console.log('Button click Completed');
+    }
+);
+
+// mouse move event
+const mousemovestream$ = Rx.Observable.fromEvent(document, 'mousemove');
+
+mousemovestream$.subscribe(
+    function (e) { // success
+        output.html('<h2>X: ' + e.clientX + ' Y: ' + e.clientY + '</h2>'); // get button Name.
+    },
+    function (err) { // error
+
+    },
+    function () { //completed
+        console.log('Button click Completed');
+    }
+);
